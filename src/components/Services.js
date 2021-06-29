831f6e42bc8a73efd9e2392e4add663eebf47b5e
Justin <justinkatasi.jk@gmail.com>
2021-06-29 15:25:26 +0200

    add

--- src/components/Services.js
+++ src/components/Services.js
@@ -17,7 +17,6 @@ const Services = () => {
                 <div className="item-image">
                     <FaCode className="FacodeService"/>
                     <h4 className="ServiceTitleCap">Développement Web </h4>
-                    <div className='underlineServiceItem'></div>
                     <p>Je créer des applications web du back-End au front-End en choisissant la technologie la plus adaptée.En association avec React pour la partie front, Node.js pour le back-End ce qui nous permet d'offrir une solution full stack basée sur un seul et même langage : javascript. </p>
                 </div>
             </li>
@@ -25,7 +24,6 @@ const Services = () => {
                 <div className="item-image">
                     <FaMobileAlt className="FacodeService"/>
                     <h4 className="ServiceTitleCap">Développement Mobile </h4>
-                    <div className='underlineServiceItem'></div>
                     <p>Grâce à React Native, je développe des applications mobiles multiplateforme iOS et Android. ce qui permet de construire une architecture solide et une experiénce utilisateur de haute qualités </p>
                 </div>
             </li>
@@ -33,7 +31,6 @@ const Services = () => {
                 <div className="item-image">
                      <FaLaptop className="FacodeService"/>
                      <h4 className="ServiceTitleCap">Design UX/UI</h4>
-                     <div className='underlineServiceItem'></div>
                      <p>Conception des interfaces utilisateur agréable par le biais duquel l'utilisateur entre en contact avec le produit. Objectif de diminuer au maximum les interrogations que peut avoir un utilisateurs lors de sa navigation sur un site web.  </p>
                 </div>
             </li>
