52a43c438c3e2a81b181716ade59b9cc755da0de
Justin <justinkatasi.jk@gmail.com>
2021-06-30 22:27:40 +0200

    add

--- src/components/Services.js
+++ src/components/Services.js
@@ -24,14 +24,14 @@ const Services = () => {
                 <div className="item-image">
                     <FaMobileAlt className="FacodeService"/>
                     <h4 className="ServiceTitleCap">Développement Mobile </h4>
-                    <p>Création des applications mobiles grâce à React Native de javascript, enfin de développer des applications mobiles multiplateforme IOS et Android. ce qui permet de construire une architecture solide et une experiénce utilisateur de haute qualités </p>
+                    <p>Création des applications mobiles grâce à React Native de javascript, afin de développer des applications mobiles multiplateformes IOS et Android. ce qui permet de construire une architecture solide et une experiénce utilisateur de haute qualitée </p>
                 </div>
             </li>
             <li className="item">
                 <div className="item-image">
                      <FaLaptop className="FacodeService"/>
                      <h4 className="ServiceTitleCap">Design UX/UI</h4>
-                     <p>Conception des interfaces utilisateur agréables par le biais duquel l'utilisateur entre en contact avec le produit. Objectif est de diminuer au maximum les interrogations que peut avoir un utilisateurs lors de sa navigation sur un site web.</p>
+                     <p>Conception des interfaces utilisateur agréables par le biais duquel l'utilisateur entre en contact avec le produit. Objectif est de diminuer au maximum les interrogations que peut avoir un utilisateur lors de sa navigation sur un site web.</p>
                 </div>
             </li>
             </ul>
--- src/pages/About.js
+++ src/pages/About.js
@@ -12,7 +12,7 @@ const About = () => {
             <div className='underlineService'></div>
          </div>
         <p className="sectionAbout">Bonjour, je m'appelle Justin Katasi et je suis un développeur full-Stack junior 
-          Je construis carrière à partir de ce que j'aime vraiment comme une passion depuis le collège. 
+          Je construis ma carrière à partir de ce que j'aime vraiment. 
           J’ai perfectionné mes compétences en tant que développeur Full-Stack Web, mobile en créant des applications Web et mobile avec Java, Python, PHP, JavaScript, React, Node, HTML, CSS, MySQL. 
         </p>
     </section>
