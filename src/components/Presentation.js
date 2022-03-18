1c4897cb37ebeae751806fff2a2249c795d15148
Justin Katasi <justinkatasi.jk@gmail.com>
2022-03-18 15:34:25 +0100

    Modification de texte descriptif à la partie Qui suis je

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -13,21 +13,19 @@ const Presentation = () => {
         <div className="hero-center">
             <article className="hero-info">
                 <div className='underline'></div>
-                <h1 className="hero-Title">Bienvenue </h1>
-                <h3 className="hero-SubTitle">Portfolio personnel</h3>
+                <h1 className="hero-Title">Bonjour et bienvenue </h1>
+                <h3 className="hero-SubTitle">sur mon portfolio personnel !</h3>
                 <br />
                 <br />
-                <h1 className="hero-Name">JUSTIN KATASI
-                </h1>
-                <p className="hero-NameSubTitle">
-                    Développeur Freelance Web et Mobile
-                </p>
-            <div>
-            <Link to={`/about`}> <button className="btnHero">Détails </button></Link>
-            <button className="btnHero">
-                <a href={monCv} target="_blank" rel="noreferrer">Mon CV</a>
-            </button>
-            </div>
+                {/*Qui suis-je*/}
+                <div>
+                    <button className="btnHeroTitle">Qui suis-je ?</button>
+                    <p>Je suis Justin Katasi, étudiant en deuxième année de BTS SIO option SLAM(Solutions Logicielles et Application Métier) au Lycée du Parc des Loges à Evry dans le 91. Passionné par le développement informatique depuis 3 ans, particulièrement les technologies Web et mobile, Je construis ma carrière à partir de ce que j'aime vraiment. Je perfectionne mes compétences en créant des applications Web et mobile à l'aide de : PHP, JavaScript, HTML, CSS, Figma, MySQL, React, Node,Java, Python... Dans le but de pouvoir acquerier des nouvelles compétences, je poursuis mes études en master dans l'ingérie du Web </p>
+                    <Link to={`/about`}> <button className="btnHero">Plus de détails </button></Link>
+                    <button className="btnHero">
+                    <a href={monCv} target="_blank" rel="noreferrer">Mon CV</a>
+                    </button>
+                </div>
             </article>
             <article className="hero-images">
                 <img src={cvImage} className="phone-img" alt="phone" />
